ae71abaf2de7cddefae97302a25ccd45dfd59e54
mkaychuks <sachufusi@gmail.com>
2021-05-12 13:46:39 -0700

    Creating a simple http server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Express tutorial')
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200)
+    res.end(`Welcome to our homepage, you sent a ${req.method} request`)
+})
+
+
+server.listen(5000, () => {
+    console.log('Server is listening at port 5000...')
+})
