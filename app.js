fa45bb9ac0f157828bce57f02fd651e7adeed94a
mkaychuks <sachufusi@gmail.com>
2021-05-12 14:04:20 -0700

    learning basics of http status codes and responses

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200)
-    res.end(`Welcome to our homepage, you sent a ${req.method} request`)
+    let url = req.url;
+    // home page
+    if(url === '/'){
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+        res.end(`<h1>Homepage</h1>`)
+    }
+    // about page
+    else if(url === '/about'){
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        })
+        res.end(`<h1> About Page</h1>`)
+    }
+    // 404 page
+    else{
+        res.writeHead(200, {
+        'content-type': 'text/html'
+        })
+        res.end(`<h1>Page not Found</h1>`)
+    }
 })
 
 
