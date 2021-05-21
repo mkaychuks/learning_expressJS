64f8dca47b24ae4a602642c3348b95ece0d5328e
mkaychuks <sachufusi@gmail.com>
2021-05-21 12:16:29 -0700

    express.json() and it's use :rocket:

--- app.js
+++ app.js
@@ -9,12 +9,24 @@ const app = express();
 // static files setup
 app.use(express.static('./methods-public'));
 app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: people})
 })
 
 
+// server side handling of forms
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
+    if( !name ){
+        return res.status(400).json({success: false, message:'Please provide a valid credential'})
+    }
+    res.status(201).send({ success: true, person: name})
+})
+
+
+/// frontend handling of forms
 app.post('/login', (req, res) => {
     const { name } = req.body;
     
