68b700e058b5f0a9c8e99a062798abee4eabed6d
mkaychuks <sachufusi@gmail.com>
2021-05-25 12:13:52 -0700

    updated the README file.. :rocket:

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 
 Learning the basics of nodeJS and expressJS. 
+This repo contains my basic understanding of expressJs, nodeJS with a minimalist view.
+
+#### learning not yet complete..
--- controllers/peopleController.js
+++ controllers/peopleController.js
@@ -3,6 +3,7 @@ const { people } = require("../data")
 
 const getPeople = (req, res) => {
     res.status(200).json({success: true, data: people})
+    console.log('Hello world, i am a working a document')
 }
 
 module.exports = { getPeople }
